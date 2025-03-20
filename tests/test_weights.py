1c1c63e8f077db04c2ea81478522c7c7f163491e
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-03-20 14:13:52 +1300

    remove temporary code

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -4,10 +4,7 @@ from pathlib import Path
 from toshi_hazard_post.logic_tree import HazardLogicTree
 import pytest
 
-if __name__ == "__main__":
-    fixtures_dir = resources.files('fixtures.end_to_end')
-else:
-    fixtures_dir = resources.files('tests.fixtures.end_to_end')
+fixtures_dir = resources.files('tests.fixtures.end_to_end')
 srm_filepath = Path(fixtures_dir) / 'srm_logic_tree_no_slab.json'
 gmcm_filepath = Path(fixtures_dir) / 'gmcm_logic_tree_medium.json'
 
@@ -31,17 +28,3 @@ def test_weights_correlations():
 
     weights = logic_tree.weights
     assert sum(weights) == pytest.approx(1.0)
-
-if __name__ == "__main__":
-    srm = SourceLogicTree.from_json(srm_filepath)
-    gmcm = GMCMLogicTree.from_json(gmcm_filepath)
-
-    logic_tree = HazardLogicTree(srm, gmcm)
-    idx = 0
-    print(logic_tree.composite_branches[idx].weight)
-    for branch in logic_tree.composite_branches[idx].branches:
-        print(f"source branch id: {branch.source_branch.tectonic_region_types} {branch.source_branch.branch_id}")
-        print(f"source branch weight: {branch.source_branch.weight}")
-        print(f"gmcm branch id {branch.gmcm_branches[0].gsim_name}")
-        print(f"gmcm branch weight {branch.gmcm_branches[0].weight}")
-        print("")
