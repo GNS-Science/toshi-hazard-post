4090abe4de8aa5f6fabf5a5c1df670fd145a52f1
chrisdicaprio <christopher.dicaprio@gmail.com>
2025-03-24 14:46:12 +1300

    correctly count number of aggregation types + test coverage

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -46,6 +46,12 @@ def branch_hashes(logic_tree):
     return logic_tree.branch_hash_table
 
 
+@pytest.fixture(scope='function')
+def branch_rates():
+    branch_rates_filepath = Path(__file__).parent / 'fixtures/calc/branch_rates.npy'
+    return np.load(branch_rates_filepath)
+
+
 # here we use value_store_all to make sure that component_branches and composite_branches.branches load into
 # ValueStore the same way
 def test_calc_composite_rates1(branch_hashes, component_rates_all):
@@ -82,20 +88,37 @@ def stats():
     )
 
 
-def test_calculate_aggs():
+def test_calculate_aggs(branch_rates):
 
-    branch_probs_filepath = Path(__file__).parent / 'fixtures/calc/branch_rates.npy'
     hazard_aggs_filepath = Path(__file__).parent / 'fixtures/calc/agg_rates.npy'
-    branch_rates = np.load(branch_probs_filepath)
     expected = np.load(hazard_aggs_filepath)
 
     weights = np.array([0.1, 0.1, 0.2, 0.3, 0.1, 0.2])
-    aggs = ['mean', 'std', 'cov', '0.6']
-    hazard_agg = calculate_aggs(branch_rates, weights, aggs)
+    agg_types = ['mean', 'std', 'cov', '0.6']
+    hazard_agg = calculate_aggs(branch_rates, weights, agg_types)
     assert np.allclose(hazard_agg, expected)
 
+    # agg_types can be in any order
     sorter = [1, 0, 3, 2]
-    aggs = list(np.array(aggs)[sorter])
+    agg_types = list(np.array(agg_types)[sorter])
     expected = expected[sorter]
-    hazard_agg = calculate_aggs(branch_rates, weights, aggs)
+    hazard_agg = calculate_aggs(branch_rates, weights, agg_types)
     assert np.allclose(hazard_agg, expected)
+
+
+# test for bug that didn't handle agg_types list that didn't include 'mean', 'std', and 'cov'
+@pytest.mark.parametrize(
+    "agg_types",
+    [
+        ['0.2', '0.9'],
+        ['mean', '0.2', '0.5', '0.9'],
+        ['mean', 'std', '0.2', '0.9'],
+        ['mean', 'std', 'cov', '0.2'],
+        ['std', '0.2'],
+        ['0.2', 'cov', '0.9'],
+    ],
+)
+def test_agg_types(branch_rates, agg_types):
+    weights = np.array([0.1, 0.1, 0.2, 0.3, 0.1, 0.2])
+    hazard_agg = calculate_aggs(branch_rates, weights, agg_types)
+    assert hazard_agg.shape == (len(agg_types), branch_rates.shape[1])
